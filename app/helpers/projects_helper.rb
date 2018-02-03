ee0c62ff6a676d3a69deb5856f5a9515d0bc8865
Larry Nesbit <larryjnesbit@gmail.com>
2018-02-03 15:16:43 -0600

    Completed exercise 7 Helpers

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,20 @@
 module ProjectsHelper
+	def format_target_pledge_amount(project)
+		if project.we_need_help?
+     		content_tag(:strong, 'We need your Help!!')
+    	else
+   			number_to_currency(project.target_pledge_amount)
+   		end
+	end
+
+	def time_remaining(project)
+		from_time=Time.now
+		to_time=project.pledging_ends_on
+		days_remaining = distance_of_time_in_words(from_time, to_time)
+		if from_time > to_time
+			content_tag(:strong, 'All Done!')
+		else
+			content_tag(:strong, days_remaining)	
+		end
+	end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+	def we_need_help?
+		target_pledge_amount.blank? || target_pledge_amount > 5000
+	end
 end
