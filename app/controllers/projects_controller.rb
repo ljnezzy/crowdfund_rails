cc7071d88d2fc55f4704049fb0cc25cea34aed68
Larry Nesbit <larryjnesbit@gmail.com>
2018-02-11 10:17:45 -0600

    Completed exercise 14 Destroy

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,5 +6,38 @@ class ProjectsController < ApplicationController
 	def show
 		@projects = Project.find(params[:id])
 	end
-	
+
+	def edit
+		@projects = Project.find(params[:id])
+	end
+
+	def update
+		@projects = Project.find(params[:id])
+		@projects.update(project_params)
+		redirect_to @projects
+	end
+
+	def new
+		@projects = Project.new
+		
+	end
+
+	def create
+		@projects = Project.new(project_params)
+		@projects.save
+		redirect_to @projects		
+	end
+
+	def destroy
+		@projects = Project.find(params[:id])
+		@projects.destroy
+		redirect_to projects_url		
+	end
+
+private
+
+	def project_params
+		project_params = params.require(:project).permit(:name, :description, :target_pledge_amount, :website, :pledging_ends_on)
+	end	
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Project.create!([
+{
+	name: "LarNes", 
+	description: "A website for freelance work",
+	target_pledge_amount: 1000.00,
+	website: "larnes.com", 
+	pledging_ends_on: "2018-02-01"
+},
+{
+	name: "Profile WebSite", 
+	description: "A Profile Website for displaying my profile", 
+	target_pledge_amount: 500.00, 
+	website: "fundmywebsite.com", 
+	pledging_ends_on: "2018-05-01"
+},
+{
+	 name: "Learning Site", 
+	 description: "A Website to prmote learning wed design", 
+	 target_pledge_amount: 2500.00,
+	 website: "larnes/learning.com", 
+	 pledging_ends_on: "2018-12-01"
+}
+
+
+
+	])
